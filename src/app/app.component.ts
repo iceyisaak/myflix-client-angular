a2e89533edc41b58e801f52e216587ef274d07cf
iceyisaak <me@iceyisaak.com>
2021-06-15 19:37:35 +0200

    Added user-login-form logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { UserRegistrationFormComponent } from './user-registration-form/user-registration-form.component';
 import { MatDialog } from '@angular/material/dialog';
+import { UserLoginFormComponent } from './user-login-form/user-login-form.component';
 
 @Component({
   selector: 'app-root',
@@ -22,9 +23,12 @@ export class AppComponent {
     )
   }
 
-  // openUserLoginDialog(): void {
-  //   this.dialog.open(
-
-  //   )
-  // }
+  openUserLoginDialog(): void {
+    this.dialog.open(
+      UserLoginFormComponent,
+      {
+        width: '280px'
+      }
+    )
+  }
 }

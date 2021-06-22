6765637ec2f84c0b3a0c32f8a4b1b50fe66add40
iceyisaak <me@iceyisaak.com>
2021-06-22 12:45:30 +0200

    Debugged movies router bug

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,14 @@ import { MovieCardComponent } from './movie-card/movie-card.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
 const appRoutes: Routes = [
-
+  {
+    path: 'welcome',
+    component: WelcomePageComponent
+  },
+  {
+    path: 'movies',
+    component: MovieCardComponent
+  }
 ]
 
 @NgModule({
